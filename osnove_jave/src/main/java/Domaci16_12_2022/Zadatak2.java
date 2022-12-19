d62056a6dbc397d9888eb114f104e59e19fc8ce0
Jana Antic <milijanaanticjana@gmail.com>
2022-12-19 13:45:55 +0100

    Domaci 16.12.2022.

---
+++ osnove_jave/src/main/java/Domaci16_12_2022/Zadatak2.java
@@ -0,0 +1,40 @@
+package Domaci16_12_2022;
+
+import java.util.Scanner;
+
+public class Zadatak2 {
+    public static void main(String[] args) {
+
+// Napisati program koji stampa 25 _ (donjih crta), pritom na svaku 5. iteraciju odstampa i novi red. Zadatak resiti pomocu FOR petlje.
+//Primer izvrsenja:
+//_ _ _ _ _
+//_ _ _ _ _
+//_ _ _ _ _
+//_ _ _ _ _
+//_ _ _ _ _
+
+        Scanner s = new Scanner(System.in);
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Unesi crtu: ");
+        String crta = s.next();
+
+        for (int i = 0; i < 25 ; i++) {
+            if (!(i == 5 || i =10  || i = 15 || i = 20 || i = 25));
+        }
+            System.out.println( " _ " );
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ osnove_jave/src/main/java/Domaci16_12_2022/Zadatak3.java
@@ -0,0 +1,34 @@
+package Domaci16_12_2022;
+
+import java.util.Scanner;
+
+public class Zadatak3 {
+    public static void main(String[] args) {
+
+    //  Napisati program koji stampa sve brojeve od 1 do 100 tako da:
+        //sve parne stampa kao negativne a
+        //sve neprane kao pozitivne brojeve
+        //                Primer izvrssenja:
+        //	   0, 1, -2, 3, -4, 5, -6, ……..
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Unesi a: ");
+        int a = s.nextInt();
+
+        for (int i = 0; i < 100 ; i++) {
+            if ( a % 2 == 0);
+        }
+        System.out.println( "- a: " + a );
+
+
+
+
+
+
+
+
+
+
+    }
+}
